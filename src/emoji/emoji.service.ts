b61cad3664b614ffd8e3d81759372afc45d2a2cb
Yury <72691412+cheatsnake@users.noreply.github.com>
2021-12-19 20:07:18 +0300

    add all basic emoji service methods

--- src/emoji/emoji.service.ts
+++ src/emoji/emoji.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from "@nestjs/common";
-import { InjectConnection, InjectModel } from "@nestjs/mongoose";
-import { Collection, connection, Connection, Model, Mongoose } from "mongoose";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
 import { Emoji, EmojiDocument } from "./schemas/emoji.schema";
 
 @Injectable()
 export class EmojiService {
     constructor(
-        @InjectModel(Emoji.name) private emojiModel: Model<EmojiDocument>,
-        @InjectConnection() private connection: Connection
+        @InjectModel(Emoji.name) private emojiModel: Model<EmojiDocument>
     ) {}
 
-    async getAll() {
-        return connection.collections;
+    async getAllEmoji(): Promise<Emoji[]> {
+        const emojis = await this.emojiModel.find({}, { _id: 0 }).exec();
+        return emojis;
+    }
+
+    async getEmojiByCategory(category: string): Promise<Emoji[]> {
+        const validCategory: string = category.replace(/_/g, " ");
+        const emojis = await this.emojiModel
+            .find({ category: validCategory }, { _id: 0 })
+            .exec();
+        return emojis;
+    }
+
+    async getEmojiByGroup(group: string): Promise<Emoji[]> {
+        const validGruop: string = group.replace(/_/g, " ");
+        const emojis = await this.emojiModel
+            .find({ group: validGruop }, { _id: 0 })
+            .exec();
+        return emojis;
     }
 }
