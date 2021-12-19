e0c6079715c9afb4140ac73e66df0bec34849b49
Yury <72691412+cheatsnake@users.noreply.github.com>
2021-12-19 20:06:22 +0300

    connect emoji-random service

--- src/emoji/emoji.module.ts
+++ src/emoji/emoji.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { EmojiRandomService } from "./emoji-random.service";
 import { EmojiController } from "./emoji.controller";
 import { EmojiService } from "./emoji.service";
 import { Emoji, EmojiSchema } from "./schemas/emoji.schema";
 
 @Module({
     controllers: [EmojiController],
-    providers: [EmojiService],
+    providers: [EmojiService, EmojiRandomService],
     imports: [
         MongooseModule.forFeature([{ name: Emoji.name, schema: EmojiSchema }]),
     ],
