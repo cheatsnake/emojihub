8ae88805aff7d125f7673eed8b79257c93e2925e
Yury <72691412+cheatsnake@users.noreply.github.com>
2021-12-19 23:46:29 +0300

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     app.setGlobalPrefix("");
     await app.listen(process.env.PORT || 3000);
 }
