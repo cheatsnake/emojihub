3c0c48571619617cae63b7d3c1a19b9295bf7934
Yury <72691412+cheatsnake@users.noreply.github.com>
2021-12-19 20:05:45 +0300

    add all basic api routes

--- src/emoji/emoji.controller.ts
+++ src/emoji/emoji.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
+import { EmojiRandomService } from "./emoji-random.service";
 import { EmojiService } from "./emoji.service";
 
 @Controller("api")
 export class EmojiController {
-    constructor(private readonly emojiService: EmojiService) {}
+    constructor(
+        private readonly emojiService: EmojiService,
+        private readonly emojiRandomServise: EmojiRandomService
+    ) {}
 
-    @Get("emoji")
-    getAll() {
-        return this.emojiService.getAll();
+    @Get("all")
+    getAllEmoji() {
+        return this.emojiService.getAllEmoji();
+    }
+
+    @Get("all/category_:category")
+    getAllEmojiInCategory(@Param("category") category: string) {
+        return this.emojiService.getEmojiByCategory(category);
+    }
+
+    @Get("all/group_:group")
+    getAllEmojiInGroup(@Param("group") group: string) {
+        return this.emojiService.getEmojiByGroup(group);
+    }
+
+    @Get("random")
+    getRandomEmoji() {
+        return this.emojiRandomServise.getRandomEmoji();
+    }
+
+    @Get("random/category_:category")
+    getRandomEmojiByCategory(@Param("category") category: string) {
+        return this.emojiRandomServise.getRandomEmojiInCategory(category);
+    }
+
+    @Get("random/group_:group")
+    getRandomEmojiByGroupInCategory(@Param("group") group: string) {
+        return this.emojiRandomServise.getRandomEmojiInGroup(group);
     }
 }
