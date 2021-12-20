ee8ea4e96dc2279025fe8865704cad3c3eb91c0e
Yury <72691412+cheatsnake@users.noreply.github.com>
2021-12-20 11:16:39 +0300

    add static file with scripts

---
+++ public/script.js
@@ -0,0 +1,17 @@
+const emoji1 = document.getElementById("1");
+const emoji2 = document.getElementById("2");
+const emoji3 = document.getElementById("3");
+
+function updateEmoji(element) {
+    fetch(
+        "http://emojihub.herokuapp.com/api/random/category_smileys_and_people"
+    )
+        .then((response) => response.json())
+        .then((emoji) => {
+            element.innerHTML = emoji.htmlCode[0];
+        });
+}
+
+setInterval(() => updateEmoji(emoji1), Math.floor(Math.random(2000) + 3000));
+setInterval(() => updateEmoji(emoji2), Math.floor(Math.random(2000) + 1000));
+setInterval(() => updateEmoji(emoji3), Math.floor(Math.random(2000) + 2000));
